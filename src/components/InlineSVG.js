6dd22dac71126bb11673694743324f68d1a3a620
Jennifer Stern <jstern@instructure.com>
2017-04-19 11:57:10 -0500

    Upgrade React and Version Bump
    refs INSTUI-441
    
    Change-Id: I76671a5302a24f0503005a4d6dc4a9c192c0f4e3
    Reviewed-on: https://gerrit.instructure.com/109077
    Reviewed-by: Pam Hiett <phiett@instructure.com>
    Product-Review: Jennifer Stern <jstern@instructure.com>
    QA-Review: Jennifer Stern <jstern@instructure.com>
    Tested-by: Jennifer Stern <jstern@instructure.com>

--- src/components/InlineSVG.js
+++ src/components/InlineSVG.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import shortid from 'shortid';
 
 export default class InlineSVG extends React.Component {
